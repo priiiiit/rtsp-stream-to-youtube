e8a66461dba4d2ea58db732c5a17b59743d67dac
Priit Pihus <priit@neocard.fi>
2016-09-20 19:06:18 +0300

    Pushing readme and first revision of the script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rtsp-stream-to-youtube
 A script that allows to stream RTSP source to Youtube live using FFMPEG.
+
+Cheatsheet for used ffmpeg parameters. Needs reformatting and additional information
+```
+ -f lavfi
+ -f anullsrc	-->	null audio source
+ -rtsp_transport tcp -i $INPUT
+ -tune zerolatency
+ -vcodec libx264 ---> Seems that mpeg4 actually works better
+ -pix_fmt +	 --->  If pix_fmt is a single +, ffmpeg selects the same pixel format as the input (or graph output) and automatic conversions are disabled.
+ -c:v copy
+ -c:a aac
+ -strict experimental 	---> Not needed anymore
+ -f flv rtmp://a.rtmp.youtube.com/live2/$1
+```
---
+++ stream-to-youtube.sh
@@ -0,0 +1,37 @@
+	#/bin/sh
+
+	# Stream rtsp to youtube
+	# Script takes one paramterer as argument, which is the stream key/code from youtube
+	# Audio input support not yet implemented
+
+	##################################
+	##         CONFIGURATION	##
+	## Change the following values	##
+	## according to yout needs	##
+	##################################
+
+	INPUT="rtsp://192.168.0.100/media/video1"
+
+	####### Changing below this line is not needed ########
+
+	if [ -z $1 ]; then
+		echo "Give the stream key as the first parameter when running the script"
+		exit
+	fi
+
+
+	echo "Input streamiks on: " $INPUT
+
+
+	ffmpeg -f lavfi \
+		-i anullsrc \
+	-rtsp_transport udp \
+	-i $INPUT \
+	-tune zerolatency \
+	-vcodec mpeg4 \
+	-pix_fmt + \
+	-c:v copy \
+	-c:a aac \
+	-f flv rtmp://a.rtmp.youtube.com/live2/$1
+
+
